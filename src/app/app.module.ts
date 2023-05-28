7fa6b95f3c650a226ad2d4c09a5cfbbe9ca60fcc
D.T. Holstein <danny.holstein.bee@outlook.com>
2023-05-28 10:49:21 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 //Services
 import { GameSettingsService } from './services/game-settings.service';
@@ -11,7 +12,6 @@ import { TreasuresService } from './services/treasures.service';
 import { AppComponent } from './app.component';
 import { EnchantedMazeComponent } from './components/enchanted-maze/enchanted-maze.component';
 import { StartNewGameComponent } from './components/start-new-game/start-new-game.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
